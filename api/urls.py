ef85b1c52dfdfc678c5a5588c211771af6e61bc4
shutsuchi <wwww.www.wshu@gmail.com>
2020-12-05 22:56:18 +0900

    add(base-api): finish-django-api

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
+from api.views import TaskViewSet, CreateUserView, MyProfileView
 
 router = routers.DefaultRouter()
+router.register('tasks', TaskViewSet, basename='tasks')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('myself', MyProfileView.as_view(), name='myself'),
+    path('register/', CreateUserView.as_view(), name='register'),
+    path('', include(router.urls)),
 ]
